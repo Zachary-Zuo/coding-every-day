c78c195e1a5918a46ff0ff87c3a16dd5b0dc8d33
Zachary <zcy621799@163.com>
2021-03-26 16:32:29 +0800

    20210326-2-fix

--- 20210326/Leetocde650. 2 Keys Keyboard.cpp
+++ 20210326/Leetocde650. 2 Keys Keyboard.cpp
@@ -4,17 +4,16 @@ public:
     int minSteps(int n)
     {
         vector<int> dp(n + 1);
-        int m = sqrt(n);
         for (int i = 2; i <= n; i++)
         {
             dp[i] = i;
-            for (int j = 2; j <= m; j++)
+            for (int j = 2; j <= sqrt(i); j++)
             {
                 if (i % j == 0)
                 {
                     dp[i] = dp[j] + dp[i / j];
+                    break;
                 }
-                break;
             }
         }
         return dp[n];
