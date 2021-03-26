256ee693841f6585d7378ac1d2083c4368207b32
Zachary <zcy621799@163.com>
2021-03-26 16:30:24 +0800

    20210326-2

---
+++ 20210326/Leetocde650. 2 Keys Keyboard.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    int minSteps(int n)
+    {
+        vector<int> dp(n + 1);
+        int m = sqrt(n);
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = i;
+            for (int j = 2; j <= m; j++)
+            {
+                if (i % j == 0)
+                {
+                    dp[i] = dp[j] + dp[i / j];
+                }
+                break;
+            }
+        }
+        return dp[n];
+    }
+};
+
+//==========================Solution 2======================================
+// 本质上是素数分解之后的素数和，因为“乘法”快于“加法”
+class Solution
+{
+public:
+    int minSteps(int n)
+    {
+        int ans = 0;
+        int d = 2;
+        while (n > 1)
+        {
+            while (n % d == 0)
+            {
+                ans += d;
+                n /= d;
+            }
+            d += 1;
+        }
+        return ans;
+    }
+};
