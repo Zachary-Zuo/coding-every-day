1168f7f1f2cbef9a27afe30b11c39c57363e2650
Zachary <zcy621799@163.com>
2021-03-26 19:08:31 +0800

    20210326-3-fix

--- 20210326/Leetcode10. Regular Expression Matching.cpp
+++ 20210326/Leetcode10. Regular Expression Matching.cpp
@@ -16,7 +16,7 @@ public:
         vector<vector<bool>>
             dp(m + 1, vector<bool>(n + 1, false));
         dp[0][0] = true;
-        for (int i = 1; i <= m; i++)
+        for (int i = 0; i <= m; i++)
         {
             for (int j = 1; j <= n; j++)
             {
