039c977da5fba5e5d7028180731696cb3477a72e
Zachary <zcy621799@163.com>
2021-04-04 20:50:59 +0800

    20210404-4

---
+++ 20210404/Leetcode128. Longest Consecutive Sequence.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int longestConsecutive(vector<int> &nums)
+    {
+        unordered_set<int> hash;
+        for (const auto &num : nums)
+        {
+            hash.insert(num);
+        }
+        int ans = 0;
+        while (!hash.empty())
+        {
+            int cur = *hash.begin();
+            hash.erase(cur);
+            int next = cur + 1;
+            int prev = cur - 1;
+            while (hash.count(next))
+            {
+                hash.erase(next++);
+            }
+            while (hash.count(prev))
+            {
+                hash.erase(prev--);
+            }
+            ans = max(ans, next - prev - 1);
+        }
+        return ans;
+    }
+};
