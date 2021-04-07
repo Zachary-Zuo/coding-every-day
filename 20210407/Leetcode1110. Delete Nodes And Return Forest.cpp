d9af8cf917cec3188fba98c0552ecf59b953e1bc
Zachary <zcy621799@163.com>
2021-04-07 16:48:28 +0800

    20210407

---
+++ 20210407/Leetcode1110. Delete Nodes And Return Forest.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<TreeNode *> delNodes(TreeNode *root, vector<int> &to_delete)
+    {
+        vector<TreeNode *> forest;
+        unordered_set<int> dict(to_delete.begin(), to_delete.end());
+        root = helper(root, dict, forest);
+        if (root)
+        {
+            forest.push_back(root);
+        }
+        return forest;
+    }
+    
+    TreeNode *helper(TreeNode *root, unordered_set<int> &dict, vector<TreeNode *> &forest)
+    {
+        if (!root)
+        {
+            return root;
+        }
+        root->left = helper(root->left, dict, forest);
+        root->right = helper(root->right, dict, forest);
+        if (dict.count(root->val))
+        {
+            if (root->left)
+            {
+                forest.push_back(root->left);
+            }
+            if (root->right)
+            {
+                forest.push_back(root->right);
+            }
+            root = NULL;
+        }
+        return root;
+    }
+};
